0524c5cbb14d1260943bf0e5e9fa93b6938d93fd
Diep1043234 <1043234@pdsb.net>
2024-09-20 09:18:41 -0400

    Update U2L2.py

--- U2L2.py
+++ U2L2.py
@@ -1,10 +1,26 @@
-print (20)
+#Modify 1
+amount = 24
+print(amount)
+print("Two dozen is: ",amount)
 
-x = 7
-x = x + 2
-print("x is: ", x)
+#Modify 2
+amount= 7*4
+print("Twenty-eight is: ", amount)
 
-q = 10
-q = q * 3  #or q*=3
-q/=3  #or q = q/3
-print("The value of q is now: ", q)
+#Modify 3
+amount = 8
+amount +=1
+print("The amount is: ", amount)
+
+#Modify 4
+amount = 8
+amount*=2
+print("The amount is: ", amount)
+
+q = 18
+q = q - 1 # using subtraction
+q -= 1 # does the same thing
+print("The value is now: ", q)
+q = q / 2 # using division
+q /= 2 # same thing
+print("The value is now: ", q) 
