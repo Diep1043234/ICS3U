7a4e8ead440da8581e9a4d1fd54926ff77796af7
Diep1043234 <1043234@pdsb.net>
2024-10-03 10:40:18 -0400

    Update U2L2.py

--- U2L2.py
+++ U2L2.py
@@ -8,3 +8,16 @@ print("Now deciding if", y, "is a factor of", x, "...")
 rem = x % y
 if rem == 0:
     print("Yes!", y, "is a factor of", x)
+
+
+#modify 
+elif:
+  y = input("Please input another whole number between 1 and 20: ")
+  y = int(y)
+  if (y<1 and y>20):
+    print("%d is outside of range. Cannot continue." %y)
+  else:
+    print("Now deciding if", y, "is a factor of", x, "...")
+  rem = x % y
+  if rem == 0:
+    print("Yes!", y, "is a factor of", x)
