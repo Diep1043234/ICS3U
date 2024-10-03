e0e7d06e0507b63c91f5cb9ebc59ed27625893ad
Diep1043234 <1043234@pdsb.net>
2024-10-03 10:51:20 -0400

    Update U2L2.py

--- U2L2.py
+++ U2L2.py
@@ -10,14 +10,20 @@ if rem == 0:
     print("Yes!", y, "is a factor of", x)
 
 
-#modify 
-elif:
+#modify
+x = input("Please input a whole number between 1 and 20: ")
+x = int(x)
+if (x<1 or x>20):
+  print("%d is outside of range. Cannot continue." %x) #the condition wont execute if x is outside of range
+else:
   y = input("Please input another whole number between 1 and 20: ")
   y = int(y)
-  if (y<1 and y>20):
-    print("%d is outside of range. Cannot continue." %y)
+  if (y<1 or y>20):
+    print("%d is outside of range. Cannot continue." %y) #the condition wont execute if y is outside of range
   else:
     print("Now deciding if", y, "is a factor of", x, "...")
   rem = x % y
   if rem == 0:
-    print("Yes!", y, "is a factor of", x)
+    print("Yes!", y, "is a factor of", x) 
+  else: 
+    print(y, "is not a factor of ", x)
