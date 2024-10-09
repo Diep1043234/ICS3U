9c90f5c39c522eb711cefb6eafadcf0003157dc1
Diep1043234 <1043234@pdsb.net>
2024-10-09 10:52:39 -0400

    Update U2L3.py

--- U2L3.py
+++ U2L3.py
@@ -7,3 +7,15 @@ while n <=6:
   print ("Your ", n, "th trianguler number is ", n*(n + 1) / 2)
   n+=1
  
+n=1
+while n <=6:
+  
+  print("Your ", n, "st trianguler number is ", n*(n + 1) / 2)
+  if n = 2:
+    print ("Your ", n, "st trianguler number is ", n*(n + 1) / 2)
+  elif n = 3:
+    print ("Your ", n, "nd trianguler number is ", n*(n + 1) / 2)
+  else:
+    print ("Your ", n, "th trianguler number is ", n*(n + 1) / 2)
+  n+=1
+ 
